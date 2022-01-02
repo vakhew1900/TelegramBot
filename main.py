08b66ca6cefe5e68600b58caa0a360fb540aaf91
vakhew1900 <antchupinin@gmail.com>
2022-01-02 19:38:48 +0300

    added start_message

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import telebot as tb
+from telebot.types import Chat
+
+
+TOKEN = '5000887285:AAHjNN6dtwvIpCYzSglXGfFDXVQ4N_NmfB8'
+botName ="MicroTigerBot"
+bot = tb.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+   picture = open("picture/MikoIino.png", 'rb')
+   bot.send_photo(message.chat.id, picture)
+   bot.send_message(message.chat.id, "Hello,<b><i>{0.first_name}</i></b>.\n It`s <b><i>{1.first_name}</i></b>. Let`s work together!!".format(message.from_user, bot.get_me()), parse_mode='html')
+
+bot.infinity_polling()
